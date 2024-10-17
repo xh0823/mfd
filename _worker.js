c858b0d2274ca8f52e889485473d266772c099aa
xh0823 <62717864+xh0823@users.noreply.github.com>
2024-10-17 20:02:51 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '1bb83c10-911e-4767-81d2-adb5177c0718';
 
-const พร็อกซีไอพีs = ['129.150.37.203',  '8.219.98.13', '8.210.92.27'];
+const พร็อกซีไอพีs = ['51.79.254.182','52.74.101.26','130.61.23.77','192.9.138.241','129.150.50.63','62.72.163.109','129.213.136.180','131.186.62.248', '140.238.158.238','193.123.69.77','140.83.86.166','141.147.163.68','194.58.56.87','129.150.37.203','18.141.204.88','202.10.42.30',  '129.154.50.159','8.210.92.27','144.24.78.162'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
